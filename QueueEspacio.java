832e4c6506429d0ecb5f26f88f574a40187bf39d
Ignacio Torres <ignacio25.it@gmail.com>
2022-10-10 16:53:59 -0300

    Añadidas las clases Cola que priorizan espacio y velocidad

---
+++ QueueEspacio.java
@@ -0,0 +1,106 @@
+public class QueueEspacio <E>{
+
+    private final static Integer defaultDimension=10;
+
+    private E [] datos;
+    private int head;
+    private int tail;
+    private int count;
+
+    public QueueEspacio(){
+        this(QueueEspacio.defaultDimension);
+    }
+    
+    @SuppressWarnings("unchecked")
+    public QueueEspacio(Integer dimension){
+        this.datos= (E[]) new Object [dimension];;
+        this.head=0;
+        this.tail=0;
+        this.count=0;
+    }
+
+    public int size(){
+        return this.count;
+    }
+
+    public boolean isEmpty(){
+        return this.count<=0;
+    }
+
+    public boolean add(E element){
+        if(this.size()>=this.datos.length){
+            throw new IllegalStateException("Cola llena..");
+        }
+
+        this.datos[this.tail]=element;
+        this.tail=this.next(this.tail);
+        ++this.count;
+        return true;
+    }
+
+    public void enqueue(E element) {
+		if(this.size()>=this.datos.length) {
+			throw new IllegalStateException("Cola llena..");
+		}
+		this.datos[this.tail] = element;
+		this.tail = next(this.tail);
+	}
+	
+	public E dequeue() {
+		if(this.size()<=0) {
+			throw new IllegalStateException("Cola vacia..");
+		}
+		E element = this.datos[this.head];
+		this.head = next(this.head);
+		return element;
+	}
+
+    private int next(int pos){
+        if(++pos>=this.datos.length){
+            pos=0;
+        }
+        return pos;
+    }
+
+    public boolean offer(E element){
+        if(this.size()>=this.datos.length){
+            return false;
+        }
+        this.datos[this.tail]=element;
+        this.tail=this.next(this.tail);
+        ++this.count;
+
+        return true;
+    }
+
+    public E pool(){
+        if(this.size()<=0){
+            return null;
+        }
+        E result=this.datos[this.head];
+        this.head=this.next(this.head);
+        --this.count;
+
+        return result;
+    }  
+
+    public E remove(){
+        if(this.size()<=0){
+            throw new IllegalStateException("Cola vacia...");
+        }
+
+        E result=this.datos[this.head];
+        this.head=this.next(this.head);
+        --this.count;
+
+        return result;
+    }
+
+    public E element(){
+        if(this.size()<=0){
+            throw new IllegalStateException("Cola vacia...");
+        }
+        return this.datos[this.head];
+    }
+
+}
